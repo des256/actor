e6d6b8e1f74e0784a7999593b9146b088af89105
Desmond Germans <desmond@germansmedia.nl>
2026-03-15 18:37:43 +0100

    update

--- docker/build-murdock.sh
+++ docker/build-murdock.sh
@@ -1,1 +1,4 @@
+cp /usr/lib/aarch64-linux-gnu/libnvdla_compiler.so libnvdla_compiler.so
+cp /usr/lib/aarch64-linux-gnu/libnvcudla.so libnvcudla.so
 docker build -f Dockerfile.murdock -t actor .
+rm libnvdla_compiler.so libnvcudla.so
