27e7f6db84a85ff20546500c97e96264468235b9
Desmond Germans <desmond@germansmedia.nl>
2026-03-16 08:49:03 +0100

    trying this

--- docker/build-murdock.sh
+++ docker/build-murdock.sh
@@ -1,4 +1,1 @@
-cp /usr/lib/aarch64-linux-gnu/nvidia/libnvdla_compiler.so libnvdla_compiler.so
-cp /usr/lib/aarch64-linux-gnu/nvidia/libnvcudla.so libnvcudla.so
 docker build -f Dockerfile.murdock -t actor .
-rm libnvdla_compiler.so libnvcudla.so
